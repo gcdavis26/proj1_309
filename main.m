52bc5c6640f15da84a6d99934593a0e622665c1a
gcdavis26 <geoffreycdavis@proton.me>
2024-10-05 16:42:21 -0400

    updated main

--- main.m
+++ main.m
@@ -7,5 +7,7 @@ rB = [6064;2942;1974]; %km, B frame
 vB = [.4;4.6;-3.2]; %km/s, B frame
 
 dcmNC = dcm3(alpha);
-dcmCB = dcm2(lambda);
-dcmNB = dcm2 * dcm3;
+dcmCB = dcm1(lambda);
+dcmNB = dcmCB * dcmNC;
+
+
