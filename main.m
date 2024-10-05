af842b4a73d93b83053544f2ce075fe4f262e40e
archangel012 <carlosandrade.trololol@gmail.com>
2024-10-05 18:03:41 -0400

    added comments

--- main.m
+++ main.m
@@ -9,14 +9,14 @@ lambda =  52; %degrees
 rB = [6064;2942;1974]; %km, B frame
 vB = [.4;4.6;-3.2]; %km/s, B frame
 
-dcmNC = dcm3(alpha);
-dcmCB = dcm1(lambda);
-dcmNB = dcmCB * dcmNC;
-dcmBN = transpose(dcmNB);
-rN = dcmBN * rB;
+dcmNC = dcm3(alpha); %generating DCMs
+dcmCB = dcm1(lambda); 
+dcmNB = dcmCB * dcmNC; %multiplying DCMs together 
+dcmBN = transpose(dcmNB); %transposing DCM
+rN = dcmBN * rB; %transforming the vectors
 vN = dcmBN * vB;
 
-formatSpec = 'alpha is %3.0f degrees\n';
+formatSpec = 'alpha is %3.0f degrees\n'; %printing results
 fprintf(formatSpec,alpha)
 
 formatSpec = 'lambda is %2.0f degrees\n';
